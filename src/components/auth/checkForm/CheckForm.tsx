2dac40d4da1bdf154aa71a5dc4f5c4d9057824ee
krawtschenko <eugenykravchenko@gmail.com>
2024-03-15 10:43:32 +0100

    feat: added check email form

---
+++ src/components/auth/checkForm/CheckForm.tsx
@@ -0,0 +1,30 @@
+import emailIcon from '@/assets/email_icon.png'
+import { Button } from '@/components/ui/button/Button'
+import { Card } from '@/components/ui/card/Card'
+import { Typography } from '@/components/ui/typography/Typography'
+
+import style from './checkForm.module.scss'
+
+type CheckFormProps = {
+  email: string
+}
+
+export const CheckForm = ({ email }: CheckFormProps) => {
+  return (
+    <Card>
+      <Typography className={style.title} variant={'h1'}>
+        Check Email
+      </Typography>
+
+      <img alt={'email'} className={style.icon} src={emailIcon} />
+
+      <Typography className={style.underTitle} variant={'body2'}>
+        We’ve sent an Email with instructions to {email}
+      </Typography>
+
+      <Button as={'a'} className={style.button} fullWidth type={'submit'}>
+        Back to Sign In
+      </Button>
+    </Card>
+  )
+}
---
+++ src/components/auth/checkForm/checkForm.stories.tsx
@@ -0,0 +1,18 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import { CheckForm } from './CheckForm'
+
+const meta = {
+  component: CheckForm,
+  tags: ['autodocs'],
+  title: 'Auth/Check Form',
+} satisfies Meta<typeof CheckForm>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Primary: Story = {
+  args: {
+    email: 'eugen@proinfo.com',
+  },
+}
