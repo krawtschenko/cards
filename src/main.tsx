da117fe6b72a0d3448ff4408c49fbffa8008ea16
krawtschenko <eugenykravchenko@gmail.com>
2024-04-03 11:37:44 +0200

    fix store

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { Provider } from 'react-redux'
 import { RouterProvider } from 'react-router-dom'
 
 import { router } from '@/routes/router'
-import { store } from '@/services/store'
+import { store } from '@/services/common/store'
 import ReactDOM from 'react-dom/client'
 
 import '@/styles/index.scss'
