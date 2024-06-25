d1fee2e0aeeb7334769678e0f50cfc0fa64cd129
krawtschenko <eugenykravchenko@gmail.com>
2024-06-25 14:24:15 +0200

    fix: baseApi.ts

--- src/services/common/baseApi.ts
+++ src/services/common/baseApi.ts
@@ -1,9 +1,6 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
-// import { baseQueryWithReauth } from './base-query-with-reauth'
-
 export const baseApi = createApi({
-  // baseQuery: baseQueryWithReauth,
   baseQuery: fetchBaseQuery({
     baseUrl: 'https://api.flashcards.andrii.es',
     credentials: 'include',
